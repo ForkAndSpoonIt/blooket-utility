f48ac76c797dc559937fe0dd6ea764d4ae7c0ad5
ForkAndSpoonIt <92403275+ForkAndSpoonIt@users.noreply.github.com>
2022-01-07 09:03:43 -0500

    Update script.js

--- wloader/script.js
+++ wloader/script.js
@@ -1,20 +1,41 @@
-(async () => {
-	window.opener.postMessage(
-    await (
-      await fetch("https://unpkg.com/react@17/umd/react.development.js")
-    ).text(),
-    "*"
-  );
-	window.opener.postMessage(
-    await (
-      await fetch("https://unpkg.com/react-dom@17/umd/react-dom.development.js")
-    ).text(),
-    "*"
-  );
-  window.opener.postMessage(
-    await (
-      await fetch("https://forkandspoonit.github.io/blooket-utility/index.js")
-    ).text(),
-    "*"
-  );
-})();
+function unWebpack(webpackArr) {
+  function stringifyFunctions(obj) {
+    let res = "{";
+    for (let key in obj) {
+      res += '"' + key + '":' + obj[key].toString() + ",";
+    }
+    res = res.substr(0, res.length - 1) + "}";
+    return res;
+  }
+
+  let resStr = "[";
+  for (const ele of webpackArr) {
+    resStr += "[[" + ele[0][0] + "]," + stringifyFunctions(ele[1]) + "],";
+  }
+  resStr = resStr.substr(0, resStr.length - 1) + "]";
+  return resStr;
+}
+
+function applyUpdatePatches(jsonp, mapping) {
+  function setFunc(id, func) {
+    let successful = false;
+    for (const chunk of jsonp)
+      for (const funcId in chunk[1])
+        if (funcId === id) {
+          chunk[1][id] = func;
+          successful = true;
+        }
+    return successful;
+  }
+
+  for (const id in mapping) {
+    if (!setFunc(id, mapping[id])) console.log("Failed to apply patch " + id);
+  }
+}
+
+function wLoad() {
+  window.onmessage = (m) => {
+    eval(m.data);
+  };
+  window.open("https://forkandspoonit.github.io/wloader/index.html");
+}

b99b18c1cc581ca6a6577ae774d23a9ab91dfb3f
ForkAndSpoonIt <92403275+ForkAndSpoonIt@users.noreply.github.com>
2022-01-07 08:21:27 -0500

    Update script.js

--- wloader/script.js
+++ wloader/script.js
@@ -13,7 +13,7 @@
   );
   window.opener.postMessage(
     await (
-      await fetch("https://thatfrueddued.github.io/blooket-utility/index.js")
+      await fetch("https://forkandspoonit.github.io/blooket-utility/index.js")
     ).text(),
     "*"
   );
